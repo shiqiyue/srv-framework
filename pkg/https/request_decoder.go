c6732bf7b9d7460669390f492767e8de4631ebb1
shiqiyue <835802084@qq.com>
2021-03-18 10:19:42 +0800

    更新request decoder

--- pkg/https/request_decoder.go
+++ pkg/https/request_decoder.go
@@ -8,7 +8,7 @@ import (
 )
 
 // 创建一般的post请求解码
-func NewSimpleJsonRequestDecoder(ctx context.Context, r *http.Request, requestModel interface{}) func(ctx context.Context, r *http.Request) (interface{}, error) {
+func NewSimpleJsonRequestDecoder(requestModel interface{}) func(ctx context.Context, r *http.Request) (interface{}, error) {
 
 	return func(ctx context.Context, r *http.Request) (interface{}, error) {
 		err := jsons.NewDecoder(r.Body).Decode(requestModel)
@@ -20,18 +20,33 @@ func NewSimpleJsonRequestDecoder(ctx context.Context, r *http.Request, requestMo
 }
 
 // 什么都不做的请求解码
-func DoNothingRequestDecoder(ctx context.Context, r *http.Request) (interface{}, error) {
+func DoNothingRequestDecoder() (interface{}, error) {
 	return nil, nil
 }
 
 // 创建一般的form请求解码
-func NewSimpleFormRequestDecoder(ctx context.Context, r *http.Request, requestModel interface{}) func(ctx context.Context, r *http.Request) (interface{}, error) {
+func NewSimpleFormRequestDecoder(requestModel interface{}) func(ctx context.Context, r *http.Request) (interface{}, error) {
 	return func(ctx context.Context, r *http.Request) (interface{}, error) {
+		err := r.ParseForm()
+		if err != nil {
+			return nil, err
+		}
 		form := r.Form
-		err := mapstructure.Decode(form, requestModel)
+		err = mapstructure.Decode(form, requestModel)
 		if err != nil {
 			return nil, err
 		}
 		return requestModel, nil
+	}
+}
+
+func NewSingleFormRequestDecoder(key string) func(ctx context.Context, r *http.Request) (interface{}, error) {
+	return func(ctx context.Context, r *http.Request) (interface{}, error) {
+		err := r.ParseForm()
+		if err != nil {
+			return nil, err
+		}
+		form := r.Form
+		return form.Get(key), nil
 	}
 }
